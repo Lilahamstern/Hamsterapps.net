661e59e38c2bb7f0b496064c10b8376a324a0298
Leo Rönnebro <leo.ronnebro@hamsterapps.net>
2020-05-03 22:28:59 +0200

    Basic landing page | under development |

--- web/src/pages/Home/App.tsx
+++ web/src/pages/Home/App.tsx
@@ -1,26 +1,18 @@
+import { Component } from 'react';
 import React from 'react';
-import logo from '../../static/logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+    title: 'hamsterapps',
+  };
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.title} is currently under development!</h1>
+        <p>Please stay tuned for a release!</p>
+      </div>
+    );
+  }
+}
