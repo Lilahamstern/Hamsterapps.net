0f61c2d642763a4e9378b35afe79c740ff641274
Lilahamstern <leo.ronnebro@hamsterapps.net>
2020-06-13 00:07:30 +0200

    Refactor and change of sql query

--- server/internal/projects/projects.go
+++ server/internal/projects/projects.go
@@ -7,18 +7,58 @@ import (
 )
 
 type Project struct {
-	ID          int64
-	Title       string
-	Description string
-	User        *users.User
+	ID          int64       `json:"id"`
+	Title       string      `json:"title"`
+	Description string      `json:"description"`
+	User        *users.User `json:"user"`
 }
 
 func (project Project) Save() int64 {
-	stmt := "INSERT INTO hamsterapps.public.projects(title,description,user_id) VALUES($1,$2,$3) RETURNING id"
-	err := database.DB.QueryRow(stmt, project.Title, project.Description, 1).Scan(&project.ID)
+	sql := "INSERT INTO hamsterapps.public.projects(title,description,user_id) VALUES($1,$2,$3) RETURNING id"
+
+	stmt, err := database.DB.Prepare(sql)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer stmt.Close()
+
+	err = stmt.QueryRow().Scan(&project.ID)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	return project.ID
+}
+
+func GetAll() []Project {
+	sql := "SELECT id, title, description FROM hamsterapps.public.projects"
+
+	stmt, err := database.DB.Prepare(sql)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer stmt.Close()
+
+	rows, err := stmt.Query()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var projects []Project
+	for rows.Next() {
+		var project Project
+		err := rows.Scan(&project.ID, &project.Title, &project.Description)
+		if err != nil {
+			log.Fatal(err)
+		}
+		projects = append(projects, project)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return projects
 }
