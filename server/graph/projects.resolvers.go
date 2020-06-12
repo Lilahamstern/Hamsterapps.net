c251acd8eff0a9d021e2068de7370c01343ac97d
Lilahamstern <leo.ronnebro@hamsterapps.net>
2020-06-12 23:14:44 +0200

    Instert of project added

--- server/graph/projects.resolvers.go
+++ server/graph/projects.resolvers.go
@@ -5,23 +5,23 @@ package graph
 
 import (
 	"context"
-	"fmt"
-
 	"github.com/lilahamstern/hamsterapps.net/server/graph/model"
+	"github.com/lilahamstern/hamsterapps.net/server/internal/projects"
+	"strconv"
 )
 
 func (r *mutationResolver) CreateProject(ctx context.Context, input model.CreateProjectInput) (*model.Project, error) {
-	var project model.Project
-	var user model.User
+	var project projects.Project
 	project.Title = input.Title
 	project.Description = input.Description
-	user.Email = "leo.ronnebro@hamsterapps.net"
-	user.Username = "Lilahamstern"
-	project.User = &user
+	projectId := project.Save()
 
-	fmt.Println(ctx)
-
-	return &project, nil
+	return &model.Project{
+		ID:          strconv.FormatInt(projectId, 10),
+		Title:       project.Title,
+		Description: project.Description,
+		User:        nil,
+	}, nil
 }
 
 func (r *queryResolver) Projects(ctx context.Context) ([]*model.Project, error) {
--- server/internal/pkg/db/postgres/postgres.go
+++ server/internal/pkg/db/postgres/postgres.go
@@ -4,17 +4,20 @@ import (
 	"database/sql"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 	"log"
+	"os"
 )
 
 var DB *sql.DB
 
 func InitDB() {
-	db, err := sql.Open("postgres", "postgres:abc@10.0.0.34:5432/hamsterapps")
+	db, err := sql.Open("postgres", os.Getenv("HAMSTERAPPS_DB_URL"))
 	if err != nil {
 		log.Panic(err)
 	}
+	//defer db.Close()
 
 	if err = db.Ping(); err != nil {
 		log.Panic(err)
@@ -31,13 +34,19 @@ func PingCheckDB() {
 
 func Migrate() {
 	PingCheckDB()
-
-	driver, _ := postgres.WithInstance(DB, &postgres.Config{})
-	m, _ := migrate.NewWithDatabaseInstance(
+	driver, err := postgres.WithInstance(DB, &postgres.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	m, err := migrate.NewWithDatabaseInstance(
 		"file://internal/pkg/db/migrations/postgres",
 		"postgres",
 		driver,
 	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
 		log.Fatal(err)
