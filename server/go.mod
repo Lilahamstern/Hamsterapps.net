fb75ada1bac8c67f673aea1d9a04468626c323bc
Lilahamstern <leo.ronnebro@hamsterapps.net>
2020-06-13 21:29:58 +0200

    uuid added for ids

--- server/go.mod
+++ server/go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/99designs/gqlgen v0.11.3
 	github.com/golang-migrate/migrate/v4 v4.11.0
+	github.com/google/uuid v1.1.1
 	github.com/lib/pq v1.7.0
 	github.com/vektah/gqlparser/v2 v2.0.1
 )
--- server/internal/pkg/db/migrations/postgres/000003_create_projects_table.up.sql
+++ server/internal/pkg/db/migrations/postgres/000003_create_projects_table.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS Projects
 (
-    id          BIGSERIAL    NOT NULL,
+    id          UUID         NOT NULL DEFAULT UUID_GENERATE_V4(),
     title       VARCHAR(127) NOT NULL UNIQUE,
     description VARCHAR(200),
-    user_id     BIGSERIAL,
+    user_id     UUID,
     FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE,
     PRIMARY KEY (id)
 )
