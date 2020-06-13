db2dc0088fc14704961b9d43e3f73c7842b3ef20
Lilahamstern <leo.ronnebro@hamsterapps.net>
2020-06-13 21:32:36 +0200

    Added uuid support instead of int for idn

--- server/internal/projects/projects.go
+++ server/internal/projects/projects.go
@@ -1,29 +1,30 @@
 package projects
 
 import (
+	"github.com/google/uuid"
 	database "github.com/lilahamstern/hamsterapps.net/server/internal/pkg/db/postgres"
 	"github.com/lilahamstern/hamsterapps.net/server/internal/users"
 	"log"
 )
 
 type Project struct {
-	ID          int64       `json:"id"`
+	ID          uuid.UUID   `json:"id"`
 	Title       string      `json:"title"`
 	Description string      `json:"description"`
 	User        *users.User `json:"user"`
 }
 
-func (project Project) Save() int64 {
-	sql := "INSERT INTO hamsterapps.public.projects(title,description,user_id) VALUES($1,$2,$3) RETURNING id"
+func (project Project) Save() uuid.UUID {
+	query := "INSERT INTO hamsterapps.public.projects(title,description,user_id) VALUES($1,$2,$3) RETURNING id"
 
-	stmt, err := database.DB.Prepare(sql)
+	stmt, err := database.DB.Prepare(query)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	defer stmt.Close()
 
-	err = stmt.QueryRow().Scan(&project.ID)
+	err = stmt.QueryRow(project.Title, project.Description, project.User.ID).Scan(&project.ID)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,9 +33,9 @@ func (project Project) Save() int64 {
 }
 
 func GetAll() []Project {
-	sql := "SELECT id, title, description FROM hamsterapps.public.projects"
+	query := "SELECT p.id, p.title, p.description, u.id, u.username, u.email FROM hamsterapps.public.projects p INNER JOIN hamsterapps.public.users u on p.user_id = u.id"
 
-	stmt, err := database.DB.Prepare(sql)
+	stmt, err := database.DB.Prepare(query)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -49,10 +50,12 @@ func GetAll() []Project {
 	var projects []Project
 	for rows.Next() {
 		var project Project
-		err := rows.Scan(&project.ID, &project.Title, &project.Description)
+		var user users.User
+		err := rows.Scan(&project.ID, &project.Title, &project.Description, &user.ID, &user.Username, &user.Email)
 		if err != nil {
 			log.Fatal(err)
 		}
+		project.User = &user
 		projects = append(projects, project)
 	}
 
